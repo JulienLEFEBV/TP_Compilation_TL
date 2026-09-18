cd4b3a082d94274337f0c06844daf0cf10d162e9
Julien Lefebvre <julienlefebvre11@hotmail.fr>
2026-09-18 17:00:47 +0200

    TP1_Ex4

--- logo/code/ex4.s
+++ logo/code/ex4.s
@@ -8,4 +8,26 @@
 
 _start:
 	reserve 1	@ 0 -> i
+	push #0
+	set_glob 0
+	push #45
+	invoke 3
+loop:
+	push #10
+	invoke 1
+	push #90
+	invoke 3
+	push #10
+	invoke 1
+	push #90
+	invoke 4
+	get_glob 0
+	push #1
+	add
+	set_glob 0
+	push #20
+	get_glob 0
+	goto_ge end_loop
+	goto loop
+end_loop:
 	stop
