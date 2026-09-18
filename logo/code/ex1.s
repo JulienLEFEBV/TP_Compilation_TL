a7abf6bb066c89ed690fc0b870ce477a7893156f
Julien Lefebvre <julienlefebvre11@hotmail.fr>
2026-09-18 16:35:56 +0200

    TP1_Ex2

--- logo/code/ex1.s
+++ logo/code/ex1.s
@@ -5,4 +5,17 @@
 @
 
 _start:
+	reserve 1
+	push #0
+	set_glob 0
+L1:
+	push #3
+	get_glob 0
+	goto_ge L2
+	push #1
+	get_glob 0
+	add
+	set_glob 0
+	goto L1
+L2:
 	stop
--- logo/code/ex2.s
+++ logo/code/ex2.s
@@ -9,6 +9,23 @@
 @
 
 _start:
-
-
+	reserve 1
+	push #0
+	set_glob 0
+	push #90
+	invoke 3
+loop:
+	push #40
+	invoke 1
+	push #120
+	invoke 4
+	push #1
+	get_glob 0
+	add
+	set_glob 0
+	push #3
+	get_glob 0
+	goto_ge end_loop
+	goto loop
+end_loop:
 	stop
